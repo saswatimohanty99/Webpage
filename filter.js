f0cf019c92a4bf53764a26956f2d941213bc22c8
saswatimohanty99 <mohantysaswati99@gmail.com>
2024-03-21 23:57:50 +0530

    filter changes

--- filter.js
+++ filter.js
@@ -1,10 +1,19 @@
 const players=["sachin","sourav","Dhoni","uv","panth","kohli"]
 const divref = document.querySelector("#list")
-function prepareList(){
-    const playersStr= players.map(function(val){
+function prepareList(data){
+    const playersStr= data.map(function(val){
         return`<li>${val}</li>`
     }).join("")
     const list=`<ul>${playersStr}</ul>`
     divref.innerHTML=list;
 }
-prepareList();
+
+function handleChange(eve){
+    const value =  eve.target.value;
+    const filteredPlayers=players.filter(function(val){
+        return val.toLowerCase().startsWith(value.toLowerCase())
+    })
+    prepareList(filteredPlayers)
+
+}
+prepareList(players); 
